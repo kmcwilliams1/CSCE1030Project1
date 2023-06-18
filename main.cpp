8389963a2b1f5fd26d4bd201744b26c69f7872d4
Kyle McWilliams <kmac52694@gmail.com>
2023-06-18 12:32:56 -0500

    pulled guessNumFunc into switch case, all logic done, now needs styling

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+// this is the enum for the menu choices
 enum MenuChoice {
     displayLeft, displayRight, guess, changeNumbers, quit
 };
@@ -27,175 +29,160 @@ istream &operator>>(istream &is, MenuChoice &choice) {
 
     return is;
 }
+
 MenuChoice choice;
 
-
-int changeNumFunc(int &leftNum, int &rightNum) {
-    do {
-        leftNum = rand() % 100 + 100;
-        rightNum = rand() % 100 + 100;
-    } while (leftNum > rightNum);
-    return leftNum && rightNum;
-};
-
-int mainMenu(){
+int mainMenu() {
     // main menu
     cout << "Enter your choice:"
          << "\n1. Display left"
          << "\n2. Display right"
          << "\n3. Guess"
          << "\n4. Change numbers"
-         << "\n5. Quit" << endl;
+         << "\n5. Quit \n" << endl;
     return choice;
 }
 
 
-int guessNumFunc(int leftNum, int rightNum, int &lastGuessNum, int &lifePoints, int &guessNum , int &displayNum) {
-
-    // this happens when the person has not displayed a number yet and points are not reduced
-    if(displayNum == 0) {
-        if (guessNum == lastGuessNum) { cout << "You just guessed that number." << endl;  return lifePoints && lastGuessNum && guessNum && displayNum && leftNum && rightNum;}
-
-        if (leftNum < guessNum && guessNum < rightNum) {
-            cout << "You guessed " << guessNum << "...\n";
-            cout << "You guessed correctly! +5" << endl;
-            lifePoints += 5;
-            cout << "Current score is: " << lifePoints << endl;
-            lastGuessNum = guessNum;
-            mainMenu();
-        }
-        else {
-            cout << "You guessed " << guessNum << "...\n";
-            cout << "You guessed incorrectly. -5" << endl;
-            lifePoints -= 5;
-            cout << "Current score is: " << lifePoints << endl;
-            lastGuessNum = guessNum;
-            mainMenu();
-        }
-    }
-
-    //this means the person has already displayed a number and the points are reduced
-    else if(displayNum != 0 ){
-        if (guessNum == lastGuessNum) { cout << "You just guessed that number." << endl;  return lifePoints && lastGuessNum && guessNum && displayNum && leftNum && rightNum;}
-
-        if (leftNum < guessNum && guessNum < rightNum) {
-            cout << "You guessed " << guessNum << "...\n";
-            cout << "You guessed correctly! +1" << endl;
-            lifePoints += 1;
-            cout << "Current score is: " << lifePoints << endl;
-            lastGuessNum = guessNum;
-            mainMenu();
-
-        }
-        else {
-            cout << "You guessed " << guessNum << "...\n";
-            cout << "You guessed incorrectly. -10" << endl;
-            lifePoints -= 10;
-            cout << "Current score is: " << lifePoints << endl;
-            lastGuessNum = guessNum;
-            mainMenu();
-        }
-    }
-
-    if (lifePoints <= 0) {
-        cout << "You have no more life points. Game over." << endl;
-        cout << "Play again? (y/n): ";
-        char playAgain;
-        cin >> playAgain;
-        if (playAgain == 'y' || playAgain == 'Y') {
-            mainMenu();
-        } else if (playAgain == 'n' || playAgain == 'N') {
-            exit(0);
-        }
-
-    }
-
-    return lifePoints && lastGuessNum && guessNum && displayNum && leftNum && rightNum;
-
-};
-
-
 int main() {
 
-
+    //declaring variables
     string playerName;
-
     int lifePoints = 100;
     int leftNum, rightNum, guessNum, lastGuessNum;
     int displayNum = 0;
-    changeNumFunc(leftNum, rightNum);
+
+    do {
+        srand(time(0));
+        leftNum = rand() % 100 + 100;
+        rightNum = rand() % 100 + 100;
+    } while (leftNum >= rightNum);
 
 
 
     // starting menu
     cout << "Enter your name: ";
     getline(cin, playerName);
-    cout << "Welcome to the game, " << playerName << endl;
+    cout << "\nWelcome to the game, " << playerName << endl;
+    cout << "*******************************" << endl;
 
 
 
+    // this combination shows the main menu and then gets the user's choice
     mainMenu();
-
+    cout << "this round..." << leftNum << " " << rightNum << " " << displayNum << endl;
     cin >> choice;
+
+
 
     // gets a user's choice
     while (choice != quit) {
         switch (choice) {
+
             case displayLeft:
 
-                if (displayNum != leftNum && displayNum != rightNum) {
+                if (displayNum != rightNum) {
                     displayNum = leftNum;
-                    cout << "The lower number is " << displayNum << endl;
-                }
-                else cout << "You already displayed a number." << endl;
+                    cout << "The lower number is " << leftNum << endl;
+                } else cout << "You already displayed a number." << endl;
+                cout << "*******************************" << endl;
                 mainMenu();
                 cin >> choice;
-
                 break;
+
+
             case displayRight:
 
-                if (displayNum != leftNum && displayNum != rightNum) {
+                if (displayNum != leftNum) {
                     displayNum = rightNum;
-                    cout << "The upper number is " << displayNum << endl;
-                }
-                else cout << "You already displayed a number." << endl;
+                    cout << "The upper number is " << rightNum << endl;
+                } else cout << "You already displayed a number." << endl;
+                cout << "*******************************" << endl;
                 mainMenu();
                 cin >> choice;
-
                 break;
-            case guess:
 
+
+            case guess:
                 cout << "Enter your guess: ";
                 cin >> guessNum;
-                guessNumFunc(leftNum, rightNum, lastGuessNum, lifePoints, guessNum, displayNum);
-                mainMenu();
-                cin >> choice;
 
+                if (guessNum == lastGuessNum) {
+                    cout << "You just guessed that number." << endl;
+                    cout << "*******************************" << endl;
+                    break;
+                }
+
+                if (displayNum == 0) {
+                    if (leftNum < guessNum && guessNum < rightNum) {
+                        cout << "You guessed " << guessNum << "...\n";
+                        cout << "You guessed correctly! +5" << endl;
+                        lifePoints += 5;
+                        cout << "Current score is: " << lifePoints << endl;
+                        lastGuessNum = guessNum;
+                        cout << "*******************************" << endl;
+
+                    } else {
+                        cout << "You guessed " << guessNum << "...\n";
+                        cout << "You guessed incorrectly. -5" << endl;
+                        lifePoints -= 5;
+                        cout << "Current score is: " << lifePoints << endl;
+                        lastGuessNum = guessNum;
+                        cout << "*******************************" << endl;
+
+                    }
+                }
+
+                //this means the person has already displayed a number and the points are reduced
+                if (displayNum == leftNum || displayNum == rightNum) {
+                    if (leftNum < guessNum && guessNum < rightNum) {
+                        cout << "You guessed " << guessNum << "...\n";
+                        cout << "You guessed correctly! +1" << endl;
+                        lifePoints += 1;
+                        cout << "Current score is: " << lifePoints << endl;
+                        lastGuessNum = guessNum;
+                        cout << "*******************************" << endl;
+
+                    } else {
+                        cout << "You guessed " << guessNum << "...\n";
+                        cout << "You guessed incorrectly. -10" << endl;
+                        lifePoints -= 10;
+                        cout << "Current score is: " << lifePoints << endl;
+                        lastGuessNum = guessNum;
+                        cout << "*******************************" << endl;
+
+                    }
+                }
                 break;
+
+
             case changeNumbers:
 
-                changeNumFunc(leftNum, rightNum);
-                cout << "Change numbers." << endl;
-                mainMenu();
-                cin >> choice;
+                do {
+                    leftNum = rand() % 100 + 100;
+                    rightNum = rand() % 100 + 100;
+                } while (leftNum > rightNum);
 
+                cout << "Change numbers." << endl;
+                cout << "*******************************" << endl;
                 break;
+
+
             case quit:
                 exit(0);
-                break;
+
+
             default:
                 cout << "Not a valid selection, sorry!" << endl;
-                mainMenu();
-                cin >> choice;
-
+                cout << "*******************************" << endl;
                 break;
         }
+
+        mainMenu();
+        cin >> choice;
     }
-
-
-    cin >> choice;
-
 
     cout << "Goodbye, World!" << endl;
     return 0;
 }
+
